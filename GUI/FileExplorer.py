bca5b3ff4dc5b28600e79eaa155b0f4397c85f6e
geschmackcr7 <trunghieuronaldo@gmail.com>
2021-11-07 03:16:36 +0700

    treefolder

--- GUI/FileExplorer.py
+++ GUI/FileExplorer.py
@@ -1,4 +1,3 @@
-from posixpath import abspath
 import socket
 from tkinter import *
 from tkinter import filedialog
@@ -13,27 +12,20 @@ class Page(Frame):
   def __init__(self, parent, soc):
     self.root = parent
     self.s = soc
-    # request = ['file explorer', "C:\\"]
-    # send_obj(soc, request)
-    # self.dirs, self.files = receive_obj(soc)
     self.nodes = {}
-    # self.dirs = [['a', 'b', 'c'], ['d', 'e', 'f']]
-    # self.files = [['r', 'b', 'a'], ['q', 'e', 'd']]
 
-    Button(self.root, text='Back', command=self.toBack).place(relx=0.04, rely=0.02, relwidth=0.1, relheight=0.05)
-    Button(self.root, text='Refresh', command=self.toRefresh).place(relx=0.16, rely=0.02, relwidth=0.1, relheight=0.05)
+    Label(self.root, text='Path: ').place(relx=0.05, rely=0.02, relwidth=0.05, relheight=0.05)
     self.path = Entry(self.root, font='Times 10')
     self.path.insert(END, "D:\\")
-    self.path.place(relx=0.28, rely=0.02, relwidth=0.62, relheight=0.05)
+    self.path.place(relx=0.12, rely=0.02, relwidth=0.5, relheight=0.05)
   
     self.frame = LabelFrame(self.root, text='File Explorer')
     self.frame.place(relx=0.04, rely=0.1, relwidth=0.9, relheight=0.85)
 
     ###  Menu
-    self.popup = Menu(self.root, tearoff=0)
+    self.popup = Menu(self.frame, tearoff=0)
     self.popup.add_command(label="Delete", command=self.delete)
     self.popup.add_command(label="Copy", command=self.copy)
-    self.popup.add_command(label="Download", command=self.download)
     #self.popup.add_separator()
 
     def do_popup(event):
@@ -46,26 +38,20 @@ class Page(Frame):
     ### Tree
     scroll = Scrollbar(self.frame, orient=VERTICAL)
     self.tree = Treeview(self.frame, yscrollcommand=scroll.set)
-    # self.tree['columns'] = ('Directory', 'Last modified time', 'Size')
-    self.tree['columns'] = ('Directory', 'Last modified time')
-    self.tree.column('#0', width=0, stretch=NO)    
-    self.tree.column('Directory', width=300, anchor=CENTER)
-    self.tree.column('Last modified time', width=50, anchor='w')
-    # self.tree.column('Size', width=50, anchor=CENTER)
+    self.tree['columns'] = ('Last modified time', 'Size')
+    self.tree.column('#0', width=450, stretch=NO, anchor='w') 
+    self.tree.column('Last modified time', width=120, anchor='w')
+    self.tree.column('Size', width=30, anchor=CENTER)
 
-    self.tree.heading('#0', text='', anchor='w')
-    self.tree.heading('Directory', text='Directory', anchor=CENTER)
+    self.tree.heading('#0', text='Directory', anchor=CENTER)
     self.tree.heading('Last modified time', text='Last modified time', anchor=CENTER)
-    # self.tree.heading('Size', text='Size', anchor=CENTER)
+    self.tree.heading('Size', text='Size', anchor=CENTER)
     self.tree.place(relx=0.02, rely=0.02, relwidth=0.92, relheight=0.96)
 
-    # self.tree.bind('<ButtonRelease-1>', self.selectItem)
-    # self.tree.bind('<Double-1>', self.openFolder)
-    # self.tree.bind('<Button-3>', do_popup)
-    # for x in range(len(self.dirs)):
-    #   self.tree.insert(parent='', index=x, iid=x, text='', values=self.dirs[x])
-    abspath = 'D:\\'
-    self.insert_node('', (abspath, ''), abspath)
+    self.tree.bind('<ButtonRelease-1>', self.selectItem)
+    self.tree.bind('<Button-3>', do_popup)
+    abspath = 'D:'
+    self.insert_node('', (abspath, ''), abspath+'\\')
     self.tree.bind('<<TreeviewOpen>>', self.open_node)
 
     scroll.config(command=self.tree.yview)
@@ -75,7 +61,7 @@ class Page(Frame):
 
   def insert_node(self, parent, value, abspath):
     # print(abspath)
-    node = self.tree.insert(parent, 'end', text=value[0], values=value, open=False)
+    node = self.tree.insert(parent, 'end', text=value[0], values=[value[1], '30kb'], open=False)
     if abspath:
       self.nodes[node] = abspath
       self.tree.insert(node, 'end')
@@ -86,19 +72,25 @@ class Page(Frame):
     # print(self.tree.item(self.tree.selection()[0])['text'])
     abspath = self.nodes.pop(node, None)
     if abspath:
-      print(abspath)
       request = ['file explorer', abspath]
       send_obj(self.s, request)
-      self.dirs, self.files = receive_obj(self.s)
+      dirs, files = receive_obj(self.s)
 
       # dirs
       self.tree.delete(self.tree.get_children(node))
-      for p in self.dirs:
+      for p in dirs:
         self.insert_node(node, p, abspath+'\\'+p[0])
 
       # files
-      for p in self.files:
+      for p in files:
         self.insert_node(node, p, None)
+    pass
+
+  def selectItem(self, event):
+    node = self.tree.focus()
+    stringPath = self.getPath(node)
+    self.path.delete(0, END)
+    self.path.insert(END, stringPath)
     pass
 
   def getPath(self, id):
@@ -107,45 +99,17 @@ class Page(Frame):
     while self.tree.item(id)['text'] != '':
       stringPath = self.tree.item(id)['text'] + '\\' + stringPath
       id = self.tree.parent(id)
-    print(stringPath)
     return stringPath
     pass
 
-  def requestPath(request):
-    pass
-
-  def toBack(self):
-    if len(self.pathList) > 1: self.pathList.pop()
-    self.path.delete(0, END)
-    stringPath = ''
-    if len(self.pathList): stringPath = self.pathList[0] + "\\"
-    for i in range(1, len(self.pathList)): stringPath = stringPath + self.pathList[i] + '\\'
-    self.path.insert(END, stringPath)
-    request = ['file explorer', stringPath]
-    self.requestPath(request)
-    pass
-
-  def toRefresh():
-    print('refresh')
-    pass
-  
-  def openFolder():
-
-    pass
-
-  def selectItem():
-    pass
-
   def delete(self):
-    print(self.popup.selection['Directory'])
-    stringPath = self.path.get() + self.popup.selection['Directory']
+    stringPath = self.path.get()
     request = ['file explorer', 'delete', stringPath]
     send_obj(self.s, request)
     pass
 
   def copy(self):
-    print(self.popup.selection)
-    stringPath = self.path.get() + self.popup.selection['Directory']
+    stringPath = self.path.get()
     request = ['file explorer', 'copy', stringPath]
     send_obj(self.s, request)
     response = receive_obj(self.s)
@@ -155,10 +119,6 @@ class Page(Frame):
     myfile = open(filename, "wb")
     myfile.write(response)
     myfile.close()
-    pass
-
-  def download(self):
-    print(self.popup.selection)
     pass
 
 # ---------------------
--- Server/Utils/screen_stream.py
+++ Server/Utils/screen_stream.py
@@ -12,8 +12,9 @@ def send_image(client, img_byte_arr):
 	while len(img_size) < 8:
 		img_size = '0' + img_size
 	client.sendall(bytes(img_size, 'utf8'))	
-	time.sleep(0.001)
+	time.sleep(0.01)
 	client.sendall(img_byte_arr)
+	time.sleep(0.01)
 
 def screen_stream(client):
     temp = pyautogui.screenshot()
@@ -24,13 +25,13 @@ def screen_stream(client):
     img_byte_arr = img_byte_arr.getvalue()
     send_image(client, img_byte_arr)
 
-	#while True:
-	#	temp = pyautogui.screenshot()
-	#	temp.save('temp_image.png')
-	#	img = PIL.Image.open("temp_image.png", mode='r')
-	#	img_byte_arr = io.BytesIO()
-	#	img.save(img_byte_arr, format="PNG")
-	#	img.seek(0)
-	#	img_byte_arr = img_byte_arr.getvalue()
-	#	client.sendall(bytes(str(sys.getsizeof(img_byte_arr)), "utf8"))
-	#	client.sendall(img_byte_arr)
+	# while True:
+	# 	temp = pyautogui.screenshot()
+	# 	temp.save('temp_image.png')
+	# 	img = PIL.Image.open("temp_image.png", mode='r')
+	# 	img_byte_arr = io.BytesIO()
+	# 	img.save(img_byte_arr, format="PNG")
+	# 	img.seek(0)
+	# 	img_byte_arr = img_byte_arr.getvalue()
+	# 	# client.sendall(bytes(str(sys.getsizeof(img_byte_arr)), "utf8"))
+		# client.sendall(img_byte_arr)
